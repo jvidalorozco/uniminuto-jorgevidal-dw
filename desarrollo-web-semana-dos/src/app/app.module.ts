41dd9be17dc6a67cbdcacf0ce932b26dc17a49cd
jvidalorozco <jorgevidal1995@hotmail.com>
2023-05-21 12:01:07 -0500

    setup hosting

--- desarrollo-web-semana-dos/src/app/app.module.ts
+++ desarrollo-web-semana-dos/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ProvidersComponent } from './providers/providers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    CustomersComponent,
+    ProvidersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: '', component:HomeComponent},
+      {path: 'customers', component:CustomersComponent},
+      {path: 'providers', component:ProvidersComponent},
+     // {path: '**', component:PageNotFoundComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
